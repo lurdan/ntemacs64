08c79f76e6be29a34bcc5d545fd7849a27c169da
KURASHIKI Satoru <lurdan@gmail.com>
2021-05-12 12:40:29 +0900

    try to build HEAD with native-comp

--- build.sh
+++ build.sh
@@ -6,21 +6,25 @@ TARGET=${2:-/c/emacs}
 WORKDIR=${3:-~/tmp}
 
 DEPLIBS="libtiff libgdk_pixbuf libglib libgobject libffi libgmp libhogweed libiconv libidn2 libintl libnettle libp11-kit libtasn1 libunistring libwinpthread"
-DEPPKGS=$(echo mingw-w64-x86_64-{toolchain,xpm-nox,libtiff,giflib,libpng,libjpeg-turbo,librsvg,libxml2,gnutls,lcms2,zlib,jansson})
+DEPPKGS=$(echo mingw-w64-x86_64-{toolchain,xpm-nox,libtiff,giflib,jbigkit,libpng,libjpeg-turbo,librsvg,libxml2,gnutls,libgccjit,cairo,lcms2,zlib,jansson,harfbuzz})
 DEPPKGS_PDF=$(echo mingw-w64-x86_64-{poppler,imagemagick})
 
 _prepare () {
-  pacman --needed --noconfirm --refresh -S base-devel unzip $DEPPKGS $DEPPKGS_PDF
+  pacman --needed --noconfirm --refresh -S base-devel unzip git $DEPPKGS $DEPPKGS_PDF
 }
 
 _fetch () {
   mkdir -p ${WORKDIR}/src
   cd ${WORKDIR}/src
-  curl -O http://ftp.gnu.org/gnu/emacs/emacs-${EMACSVER}.tar.gz
-  curl -O http://ftp.gnu.org/gnu/emacs/emacs-${EMACSVER}.tar.gz.sig
+#  curl -O http://ftp.gnu.org/gnu/emacs/emacs-${EMACSVER}.tar.gz
+#  curl -O http://ftp.gnu.org/gnu/emacs/emacs-${EMACSVER}.tar.gz.sig
 
+  git config core.autocrlf false
+  git clone --depth 1 git://git.savannah.gnu.org/emacs.git .
+
+  patch -p 1 < ../emacs-28-disable-w32-ime.diff
   curl -O https://raw.githubusercontent.com/mhatta/emacs-27-x86_64-win-ime/master/cmigemo-1.3-mingw64-20180629.zip
-  curl -fsSL -o gnutls.zip 'https://gitlab.com/gnutls/gnutls/-/jobs/491918191/artifacts/download?file_type=archive'
+#  curl -fsSL -o gnutls.zip 'https://gitlab.com/gnutls/gnutls/-/jobs/491918191/artifacts/download?file_type=archive'
 }
 
 _extract () {
@@ -31,7 +35,7 @@ _extract () {
 _build () {
   #autoconf
   ./autogen.sh
-  PKG_CONFIG_PATH=/mingw64/lib/pkgconfig/ CFLAGS='-O2 -march=x86-64 -mtune=generic -static -s -g0' \
+  PKG_CONFIG_PATH=/mingw64/lib/pkgconfig/ CFLAGS='-O2 -march=x86-64 -mtune=generic -s -g0' \
     LDFLAGS='-s' ./configure --prefix=$TARGET \
                 --host=x86_64-w64-mingw32 \
                 --with-wide-int \
@@ -41,7 +45,8 @@ _build () {
                 --without-dbus \
                 --without-compress-install \
                 --with-lcms2 \
-                --with-modules && make bootstrap
+                --with-native-compilation \
+                --with-modules && make
 }
 
 _install () {
@@ -67,16 +72,16 @@ _pdftools () {
 
 _dlls () {
   cd ${WORKDIR}
-  unzip src/gnutls.zip
-  cp win64-build/bin/*.dll ${TARGET}/bin
+#  unzip src/gnutls.zip
+#  cp win64-build/bin/*.dll ${TARGET}/bin
 }
 
 _prepare
 _fetch
-_extract
+#_extract
 _build
 _install
 _cmigemo
-_pdftools
+#_pdftools
 _dlls
 
